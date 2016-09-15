4b04fcf863c9c641ce5a43d9f82a8ce155739772
Michael Byrne <Michael.Byrne@hci.utah.edu>
2016-09-15 14:51:55 -0600

    with boot1.1.3

--- src/grid/cell/cell.module.ts
+++ src/grid/cell/cell.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-//import { DropdownModule } from "ng2-bootstrap/ng2-bootstrap";
-//import { DatepickerModule } from "ng2-bootstrap/ng2-bootstrap";
+import { DropdownModule } from "ng2-bootstrap/ng2-bootstrap";
+import { DatepickerModule } from "ng2-bootstrap/ng2-bootstrap";
 
 import { LabelCell } from "./label-cell.component";
 import { InputCell } from "./input-cell.component";
@@ -10,7 +10,7 @@ import { DateCell } from "./date-cell.component";
 import { GroupCollapseExpandCell } from "./group-collapse-expand.component";
 
 @NgModule({
-    imports: [ CommonModule, FormsModule ],
+    imports: [ CommonModule, FormsModule, DropdownModule, DatepickerModule ],
     declarations: [ LabelCell, InputCell, DateCell, GroupCollapseExpandCell ],
     bootstrap: [ LabelCell, InputCell, DateCell, GroupCollapseExpandCell ]
 })
