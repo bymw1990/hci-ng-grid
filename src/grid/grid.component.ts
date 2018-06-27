dcf00866f02d59fac20938367a0a718fd590d703
Michael Byrne <u6001934@hci.utah.edu>
2018-06-27 13:53:17 -0600

    Fix focuser styling

--- src/grid/grid.component.ts
+++ src/grid/grid.component.ts
@@ -63,7 +63,7 @@ import {Observable} from "rxjs/Observable";
          (mousemove)="mouseDrag($event)"
          (dblclick)="dblClick($event)"
          (keydown)="keyDown($event)">
-      <input #focuser1 id="focuser1" style="position: absolute; left: -1000px;" (focus)="onFocus($event)" />
+      <input #focuser1 id="focuser1" style="position: absolute; left: -10000px; top: -10000px; width: 0px; height: 0px;" (focus)="onFocus($event)" />
       <textarea #copypastearea style="position: absolute; left: -2000px;"></textarea>
       
       <!-- Title Bar -->
@@ -154,7 +154,7 @@ import {Observable} from "rxjs/Observable";
         
       </div>
 
-      <input #focuser2 id="focuser2" style="position: absolute; left: -1000px;" (focus)="onFocus($event)" />
+      <input #focuser2 id="focuser2" style="position: absolute; left: -10000px; top: -9900px; width: 0px; height: 0px;" (focus)="onFocus($event)" />
       
       <!-- Footer -->
       <div *ngIf="pageInfo.pageSize > 0"
