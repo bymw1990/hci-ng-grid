7ec52d77be7975a067a20db615dcc94d1b9e40c8
Michael Byrne <Michael.Byrne@hci.utah.edu>
2019-05-29 19:34:09 -0600

    Fix dropdown

--- demo/src/app/demo.component.ts
+++ demo/src/app/demo.component.ts
@@ -22,9 +22,9 @@ declare const VERSION: string;
         <div class="mr-3">
           <button class="btn btn-outline-primary white" routerLink="/docs">Documentation</button>
         </div>
-        <div ngbDropdown placement="bottom-right" class="d-inline-block">
+        <div ngbDropdown class="d-inline-block">
           <button class="btn btn-outline-primary white" id="routeList" ngbDropdownToggle>Select A Demo</button>
-          <div ngbDropdownMenu class="dropdown-menu" style="max-height: 400px; overflow-y: auto;" aria-labelledby="routeList">
+          <div ngbDropdownMenu class="dropdown-menu" style="max-height: 400px; overflow-y: auto; left: auto; right: 0;" aria-labelledby="routeList">
             <a class="dropdown-item" routerLink="/">Home</a>
             <a class="dropdown-item" routerLink="/alerts">Alerts</a>
             <a class="dropdown-item" routerLink="/busy">Busy</a>
